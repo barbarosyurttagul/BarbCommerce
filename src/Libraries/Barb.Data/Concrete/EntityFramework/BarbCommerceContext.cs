116a8ae32d69a0a781849302bbb654e72f52a9ab
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2022-07-06 00:53:28 +0300

    EFCore Context created

---
+++ src/Libraries/Barb.Data/Concrete/EntityFramework/BarbCommerceContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Barb.Core.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barb.Data.Concrete.EntityFramework
+{
+	public class BarbCommerceContext : DbContext
+	{
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public BarbCommerceContext(DbContextOptions<BarbCommerceContext> options) : base(options)
+        {
+
+        }
+    }
+}
+
