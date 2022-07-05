a011d145d6f5e155239c15a7430a726e9b7ef6d7
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2022-07-05 23:32:21 +0300

    Barb.Data created

--- src/Libraries/Barb.Core/Domain/Catalog/Category.cs
+++ src/Libraries/Barb.Core/Domain/Catalog/Category.cs
@@ -1,11 +1,41 @@
 ﻿using System;
+using Barb.Core.Domain.Common;
+using Barb.Core.Domain.Localization;
+using Barb.Core.Domain.Seo;
+
 namespace Barb.Core.Domain.Catalog
 {
-	public class Category
-	{
-		public Category()
-		{
-		}
-	}
+	public partial class Category : BaseEntity, ILocalizedEntity, ISlugSupported, ISoftDeletedEntity
+    {
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is published
+        /// </summary>
+        public bool Published { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity has been deleted
+        /// </summary>
+        public bool Deleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of product creation
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of product update
+        /// </summary>
+        public DateTime UpdatedOnUtc { get; set; }
+    }
 }
 
